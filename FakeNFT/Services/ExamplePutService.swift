5607e5d2a4d05aba3e6a46cd24ccee37e2888611
Satkov <g.satkov@mail.ru>
2025-02-10 16:44:16 +0300

    add lint error

--- FakeNFT/Services/ExamplePutService.swift
+++ FakeNFT/Services/ExamplePutService.swift
@@ -17,6 +17,8 @@ final class ExamplePutServiceImpl: ExamplePutService {
         self.networkClient = networkClient
     }
 
+
+    
     func sendExamplePutRequest(
         param1: String,
         param2: String,
