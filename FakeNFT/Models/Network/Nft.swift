7fa17fcf02093ec959aa0188b90e3b809a9e0fa0
Satkov <g.satkov@mail.ru>
2025-02-23 20:29:27 +0300

    add models

--- FakeNFT/Models/Network/Nft.swift
+++ FakeNFT/Models/Network/Nft.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 struct Nft: Decodable {
+    let createdAt: String
+    let name: String
+    let images: [String]
+    let rating: Int
+    let description: String
+    let price: Float
+    let author: String
     let id: String
-    let images: [URL]
 }
