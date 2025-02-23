e9e79e1f03b9a0312b1cac1f78ecfc4b85d915dc
Satkov <g.satkov@mail.ru>
2025-02-23 20:32:39 +0300

    get cart data in presenter

--- FakeNFT/Scenes /Cart/CartPresenter.swift
+++ FakeNFT/Scenes /Cart/CartPresenter.swift
@@ -3,14 +3,87 @@ import UIKit
 protocol CartPresenterProtocol: AnyObject, UITableViewDelegate, UITableViewDataSource {
 }
 
+enum CartState {
+    case initial, loading, failed(Error), data([Nft])
+}
+
 class CartPresenter: NSObject {
     weak var view: CartViewProtocol?
     var router: CartRouterProtocol
     var interactor: CartInteractorProtocol
 
+    private let dispatchGroup = DispatchGroup()
+    private var orderItems: Order?
+    private var nftsInCart: [Nft] = []
+    private var state: CartState = CartState.initial {
+        didSet {
+            stateChanged()
+        }
+    }
+
     init(interactor: CartInteractorProtocol, router: CartRouterProtocol) {
         self.interactor = interactor
         self.router = router
+        super.init()
+        loadCartItems()
+    }
+
+    private func stateChanged() {
+        switch state {
+        case .initial:
+            assertionFailure("can't move to initial state")
+        case .loading:
+            print("loading")
+            // показать лоудер
+        case .failed(let error):
+            print(error)
+            // показать алерт об ошибке
+        case .data(let nft):
+            print(nft)
+            // отфильтровать
+            // перезагрузить таблицу
+            // сказать контроллеру отрисовать таблицу
+        }
+    }
+
+    private func loadCartItems() {
+        state = .loading
+        getOrder()
+        dispatchGroup.notify(queue: DispatchQueue.global()) { [weak self] in
+            guard let self else { return }
+            self.getNfts()
+        }
+    }
+
+    private func getOrder() {
+        dispatchGroup.enter()
+        interactor.getNFTInsideCart() { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let order):
+                self.orderItems = order
+            case .failure(let error):
+                state = .failed(error)
+            }
+            dispatchGroup.leave()
+        }
+    }
+
+    private func getNfts() {
+        guard let orderItems else { return }
+        nftsInCart = []
+        for id in orderItems.nfts {
+            interactor.getNFTByID(id: id) { [weak self] result in
+                guard let self else { return }
+                switch result {
+                case .success(let nft):
+                    nftsInCart.append(nft)
+                    state = .data(nftsInCart)
+                case .failure(let error):
+                    state = .failed(error)
+                }
+            }
+        }
     }
 }
 
