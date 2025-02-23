a3909693785a63455faf8d31ed017d96c6eccd47
Satkov <g.satkov@mail.ru>
2025-02-23 20:30:44 +0300

    add network services

--- FakeNFT/Services/NftService.swift
+++ FakeNFT/Services/NftService.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 typealias NftCompletion = (Result<Nft, Error>) -> Void
+typealias OrderCompletion = (Result<Order, Error>) -> Void
 
 protocol NftService {
-    func loadNft(id: String, completion: @escaping NftCompletion)
+    func loadCart(completion: @escaping OrderCompletion)
+    func getNFTById(id: String, completion: @escaping NftCompletion)
 }
 
 final class NftServiceImpl: NftService {
@@ -23,6 +25,36 @@ final class NftServiceImpl: NftService {
         }
 
         let request = NFTRequest(id: id)
+        networkClient.send(request: request, type: Nft.self) { [weak storage] result in
+            switch result {
+            case .success(let nft):
+                storage?.saveNft(nft)
+                completion(.success(nft))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func loadCart(completion: @escaping OrderCompletion) {
+        let request = NetworkRequests.getNFTInsideCart()
+        networkClient.send(request: request, type: Order.self) { result in
+            switch result {
+            case .success(let order):
+                completion(.success(order))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func getNFTById(id: String, completion: @escaping NftCompletion) {
+        if let nft = storage.getNft(with: id) {
+            completion(.success(nft))
+            return
+        }
+
+        let request = NetworkRequests.getNFTById(id: id)
         networkClient.send(request: request, type: Nft.self) { [weak storage] result in
             switch result {
             case .success(let nft):
