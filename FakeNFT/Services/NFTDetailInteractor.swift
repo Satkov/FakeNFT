8d74b2c30007dc0b122d90c770af0272ae4026a0
Nikolay Shpilevskiy <infnick@list.ru>
2025-03-04 16:53:35 +0500

    Remove unnecessary methods from nft detail interactor

--- FakeNFT/Services/NFTDetailInteractor.swift
+++ FakeNFT/Services/NFTDetailInteractor.swift
@@ -5,9 +5,7 @@
 //  Created by Nikolay on 20.02.2025
 //
 
-typealias NftDetailCompletion = (Result<Nft, Error>) -> Void
 typealias CurrenciesCompletion = (Result<[Currency], Error>) -> Void
-typealias UserCompletion = (Result<User, Error>) -> Void
 
 protocol NFTDetailInteractorProtocol: AnyObject {
 }
@@ -27,36 +25,12 @@ class NFTDetailInteractor: NFTDetailInteractorProtocol {
         self.nftStorage = nftStorage
     }
     
-    func loadNft(id: String, completion: @escaping NftDetailCompletion) {
-        let request = NFTDetailRequest(id: id)
-        networkClient.send(request: request, type: Nft.self) { result in
-            switch result {
-            case .success(let nft):
-                completion(.success(nft))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
     func loadCurrencies(completion: @escaping CurrenciesCompletion) {
         let request = CurrenciesRequest()
         networkClient.send(request: request, type: [Currency].self) { result in
             switch result {
             case .success(let currencies):
                 completion(.success(currencies))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    func loadUser(completion: @escaping UserCompletion) {
-        let request = CurrenciesRequest()
-        networkClient.send(request: request, type: User.self) { result in
-            switch result {
-            case .success(let user):
-                completion(.success(user))
             case .failure(let error):
                 completion(.failure(error))
             }
